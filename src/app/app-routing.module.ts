3e91a3636b739fe6aff0a8b222777cc35c9219a3
pacokris <pacokris@yahoo.fr>
2018-11-06 22:21:42 +0100

    ajout des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CarteComponent } from './carte/carte.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReserveComponent } from './reserve/reserve.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'carte', component: CarteComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'reserve', component: ReserveComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
