d6ea4866a41f73fefa49210fbb3ab442cd12c374
pacokris <pacokris@yahoo.fr>
2018-11-07 15:11:19 +0100

    Ajout composant réserve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatDividerModule } from '@angular/material';
-import { HomeComponent } from './home/home.component';
-import { CarteComponent } from './carte/carte.component';
-import { ReserveComponent } from './reserve/reserve.component';
-import { ContactComponent } from './contact/contact.component';
-import { MainDashComponent } from './main-dash/main-dash.component';
-import { DishTableComponent } from './dish-table/dish-table.component';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MainNavComponent } from "./main-nav/main-nav.component";
+import { LayoutModule } from "@angular/cdk/layout";
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatDividerModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  MatGridListModule,
+  MatMenuModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MAT_DATE_LOCALE,
+  MatSelectModule
+} from "@angular/material";
+import { HomeComponent } from "./home/home.component";
+import { CarteComponent } from "./carte/carte.component";
+import { ReserveComponent } from "./reserve/reserve.component";
+import { ContactComponent } from "./contact/contact.component";
+import { MainDashComponent } from "./main-dash/main-dash.component";
+import { DishTableComponent } from "./dish-table/dish-table.component";
 
 @NgModule({
   declarations: [
@@ -30,20 +51,28 @@ import { DishTableComponent } from './dish-table/dish-table.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MatCardModule,
+    ReactiveFormsModule,
+    FormsModule,
     MatToolbarModule,
     MatButtonModule,
+    MatDividerModule,
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
     MatGridListModule,
     MatMenuModule,
     MatTableModule,
     MatPaginatorModule,
-    MatDividerModule,
-    MatSortModule
+    MatSortModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: MAT_DATE_LOCALE, useValue: 'fr-FR'},],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
